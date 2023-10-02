a9ee246eb74f25c80bdc81fa9b06f895d990e45f
Anghela Rodriguez <anghela.rodriguez@vallegrande.edu.pe>
2023-10-02 12:03:43 -0500

    Servicios

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { IProduct } from 'src/app/interfaces/product.interfaces';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+
+  products: IProduct [] = []
+
+  constructor(private productService: ProductService){}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.productService.findAll().subscribe(res => {
+      console.log(res);
+      this.products = res;
+      
+    });
+  }
 
 }
